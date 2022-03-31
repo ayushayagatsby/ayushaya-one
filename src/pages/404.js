b03722f09d8565f8d83845db2cf1a5147dbb3ac3
Marco Emmanuel Noto <marcoemmanuelnoto@gmail.com>
2022-03-31 13:33:20 +0200

    fixing 404 bug

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,12 +4,10 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const NotFoundPage = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
+const NotFoundPage = () => {
 
   return (
-    <Layout location={location} title={siteTitle}>
-      <Seo title="404: Not Found" />
+    <Layout>
       <h1>404: Not Found</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </Layout>
@@ -18,12 +16,4 @@ const NotFoundPage = ({ data, location }) => {
 
 export default NotFoundPage
 
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
+
