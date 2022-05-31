ee9b503e5ff31da3d95b09b515bfb1b3ec0ebb0f
Marco Emmanuel Noto <marcoemmanuelnoto@gmail.com>
2022-05-31 15:17:58 +0200

    wired cms to meta pages

---
+++ src/components/MetaPageDesktop.js
@@ -0,0 +1,128 @@
+import React, {useEffect, useState, useContext} from 'react'
+import {graphql, Link} from "gatsby"
+import {BLOCKS, MARKS} from "@contentful/rich-text-types"
+import {GatsbyImage, getImage} from 'gatsby-plugin-image'
+import {renderRichText} from "gatsby-source-contentful/rich-text"
+import {css, jsx} from '@emotion/react'
+import styled from "@emotion/styled"
+import Seo from './seo'
+import LanguageContext from '../context/LanguageContext';
+import IcArrowDown from '../assets/IcArrowDown.svg'
+import LanguageSelect from './LanguageSelect/LanguageSelect'
+
+const Bold = ({children}) => <span className="bold">{children}</span>
+const Text = ({children}) => <p className="p-big">{children}</p>
+
+const Wrapper = styled("div")`
+min-height: 100vh;
+width: 80%;
+margin: 0 auto;
+`
+
+const ProgressBarContainer = styled("div")`
+position: sticky;
+top: 0;
+background-color: #fff;
+height: 9.5vh;
+align-self: flex-start;
+border-bottom-left-radius: 20px;
+border-bottom-right-radius: 20px;
+z-index: 3;
+display: flex;
+align-items: center;
+`
+
+const ProgressBar = styled("div")`
+height: 6px;
+width: 0;
+min-width: 10px;
+max-width: 100%;
+background-color: #D4C1BA;
+position: absolute;
+top: 0;
+`
+
+const Spacer = styled("div")`
+width: 100%;
+height: 1200px;`
+
+const options = {
+    renderMark: {
+        [MARKS.BOLD]: text => <Bold>{text}</Bold>
+    },
+    renderNode: {
+        [BLOCKS.PARAGRAPH]: (node, children) => {
+            if (node.content[0].value === "") {
+                return (<br/>)
+            } else 
+                return (
+                    <Text>{children}</Text>
+                )
+        },
+        [BLOCKS.HEADING_1]: (node, children) => {
+            return (
+                <h1 css={css `margin: 5% 0 3% 0;`}>{children}</h1>
+            )
+        },
+        [BLOCKS.HEADING_2]: (node, children) => {
+            return (
+                <h2 css={css `margin-bottom: 3%;`}>{children}</h2>
+            )
+        },
+
+        [BLOCKS.EMBEDDED_ASSET]: node => {
+            return (
+                <React.Fragment>
+                    <h2>Embedded Asset</h2>
+                    <pre>
+            <code>{JSON.stringify(node, null, 2)}</code>
+          </pre>
+                </React.Fragment>
+            )
+        },
+        [BLOCKS.EMBEDDED_ASSET]: node => {
+            const {gatsbyImageData} = node.data.target
+            if (!gatsbyImageData) {
+                // asset is not an image
+                return null
+            }
+            return <GatsbyImage image={getImage(gatsbyImageData)} css={css `margin: 5% 0;`}/>
+        }
+    }
+}
+
+export default function MetaPageDesktop({data}) {
+
+    const {language} = useContext(LanguageContext)
+
+    const englishContent = data.contentfulMetaPage.englishBody;
+    const italianContent = data.contentfulMetaPage.italianBody;
+    const germanContent = data.contentfulMetaPage.germanBody;
+
+    let handleCurrentLanguage = (language) => {
+        switch (language) {
+            case 'it':
+                return italianContent
+            case 'en':
+                return englishContent
+
+            case 'de':
+                return germanContent
+
+            default:
+                break;
+        }
+    }
+
+    let currentLanguage = handleCurrentLanguage(language);
+
+    let title = data.contentfulMetaPage.englishTitle;
+
+    return (
+
+        <Wrapper>
+            <Seo title={title}/> 
+            {currentLanguage && <div css={css `padding: 10% 5%;`}>{renderRichText(currentLanguage, options)}</div>}
+        </Wrapper>
+    )
+}
--- src/pages/imprint.js
+++ src/pages/imprint.js
@@ -1,153 +1,38 @@
 import React, {useContext} from 'react';
-import {css, jsx} from '@emotion/react'
-import LanguageContext from "../context/LanguageContext"
+import {css, jsx} from '@emotion/react';
+import {graphql} from 'gatsby';
+import MetaPageDesktop from '../components/MetaPageDesktop';
 import {useBreakpoint} from 'gatsby-plugin-breakpoints';
+import MetaPageMobile from '../components/MetaPageMobile';
 
-export default function Imprint() {
+export default function Imprint({data}) {
     const breakpoints = useBreakpoint();
 
-    const {language} = useContext(LanguageContext)
-
-    let handleCurrentLanguage = (language) => {
-        switch (language) {
-            case 'it':
-                return italianContent
-                break;
-            case 'en':
-                return englishContent
-                break;
-
-            case 'de':
-                return germanContent
-                break;
-
-            default:
-                break;
-        }
-    }
-
-    let italianContent = `Improntaœ
-    Informazioni secondo il § 5 TMGœ
-    Juliane Merckensœ
-    via Mulino 1œ
-    21010 Castelveccana (VA), Italiaœ
-    Contattaœ
-    telefono: (+39) 345 723 30 90œ
-    E-mail: info@ayushaya.coachœ
-    ID IVAœ
-    Numero di identificazione dell'imposta sulle vendite secondo il § 27 a Umsatzsteuergesetz:œ
-    Ust.-ID: completare quiœ
-    Autorità di vigilanzaœ
-    Autorità di vigilanza:œ
-    Indirizzo:œ
-    http://œ
-    Titolo professionale e regolamenti professionaliœ
-    Titolo professionale:œ
-    Titolo professionale statutario:œ
-    Camera competente:œ
-    Nome e indirizzo della camera competente:œ
-    Premiato in:œ
-    Inserisci qui il paese in cui hai ottenuto il tuo titolo professionale.œ
-    Si applicano i seguenti regolamenti professionali:œ
-    2 / 4œ
-    Dettagli dell'assicurazione di responsabilità civile professionaleœ
-    Editorialmente responsabileœ
-    Operatore del sito webœ
-    Risoluzione delle controversie UEœ
-    La Commissione europea fornisce una piattaforma per la risoluzione delle controversie online (OS):œ
-    https://ec.europa.eu/consumers/odr/.œ
-    Potete trovare il nostro indirizzo e-mail nell'impronta qui sopra.œ
-    Risoluzione delle controversie dei consumatori/servizio universale di risoluzione delle controversieœ
-    Non siamo disposti o obbligati a partecipare a procedimenti di risoluzione delle controversie davanti a unœ
-    commissione per l'arbitrato dei consumatori.œ`
-
-    let germanContent = `Impressumœ
-    Angaben gemäß § 5 TMGœ
-    Juliane Merckensœ
-    via Mulino 1œ
-    21010 Castelveccana (VA), Italienœ
-    Kontaktœ
-    Telefon: (+39) 345 723 30 90œ
-    E-Mail: info@ayushaya.coachœ
-    Umsatzsteuer-ID
-    Umsatzsteuer-Identifikationsnummer gemäß § 27 a Umsatzsteuergesetz:œ
-    Ust.-ID: hier ergänzenœ
-    Aufsichtsbehördeœ
-    Aufsichtsbehörde:œ
-    Anschrift:œ
-    http://œ
-    Berufsbezeichnung und berufsrechtliche Regelungenœ
-    Berufsbezeichnung:œ
-    gesetzl. Berufsbezeichnung:œ
-    Zuständige Kammer:œ
-    Name und Anschrift der zuständigen Kammer:œ
-    Verliehen in:œ
-    Tragen Sie hier das Land ein, in dem Ihnen Ihre Berufsbezeichnung verliehen wurde.œ
-    Es gelten folgende berufsrechtliche Regelungen:œ
-    2 / 4œ
-    Angaben zur Berufshaftpflichtversicherungœ
-    Redaktionell verantwortlichœ
-    Betreiber der Webseiteœ
-    EU-Streitschlichtungœ
-    Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit:œ
-    https://ec.europa.eu/consumers/odr/.œ
-    Unsere E-Mail-Adresse finden Sie oben im Impressum.œ
-    Verbraucherstreitbeilegung/Universalschlichtungsstelleœ
-    Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einerœ
-    Verbraucherschlichtungsstelle teilzunehmen.œ
-    `
-
-    let englishContent = `Imprintœ
-    Information according to § 5 TMGœ
-    Juliane Merckensœ
-    via Mulino 1œ
-    21010 Castelveccana (VA), Italyœ
-    Contactœ
-    phone: (+39) 345 723 30 90œ
-    E-mail: info@ayushaya.coachœ
-    VAT IDœ
-    Sales tax identification number according to § 27 a Umsatzsteuergesetz:œ
-    Ust.-ID: complete hereœ
-    Supervisory Authorityœ
-    Supervisory Authority:œ
-    Address:œ
-    http://œ
-    Professional title and professional regulationsœ
-    Professional title:œ
-    Statutory professional title:œ
-    Competent chamber:œ
-    Name and address of the competent chamber:œ
-    Awarded in:œ
-    Enter here the country in which you were awarded your professional title.œ
-    The following professional regulations apply:œ
-    2 / 4œ
-    Details of professional liability insuranceœ
-    Editorially responsibleœ
-    Operator of the websiteœ
-    EU Dispute Resolutionœ
-    The European Commission provides a platform for online dispute resolution (OS):œ
-    https://ec.europa.eu/consumers/odr/.œ
-    You can find our e-mail address in the imprint above.œ
-    Consumer dispute resolution/universal dispute resolution serviceœ
-    We are not willing or obligated to participate in dispute resolution proceedings before aœ
-    consumer arbitration board.œ`
-
-    let currentLanguage = handleCurrentLanguage(language);
-
-    let textIntoArray = currentLanguage.split("œ");
-
-    const toRender = textIntoArray
-        .slice(1, -1)
-        .map(line => (
-            <p>{line}<br/></p>
-        ))
-
     return (
-        <div
-            css={css `margin-top: ${breakpoints.sm
-            ? ""
-            : "20vh"}`}>
-            <h1 css={css`margin-bottom: 70px;`}>{textIntoArray[0]}</h1>
-            {toRender}</div>
+        <React.Fragment>
+            {breakpoints.sm && <MetaPageMobile data={data}/>}
+            {(!breakpoints.md && breakpoints.l) && <MetaPageDesktop data={data}/>}
+        </React.Fragment>
     )
 }
+
+export const query = graphql `
+  query ImprintQuery {
+    contentfulMetaPage(slug: {eq: "imprint"}) {
+      id
+      slug
+      italianBody {
+        raw
+      }
+      germanBody {
+        raw
+      }
+      englishBody {
+        raw
+      }
+      englishTitle
+      germanTitle
+      italianTitle
+    }
+  }
+`
