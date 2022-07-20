77027849f38e59624de4e1cb0cbf85fffde6d3e5
Marco Emmanuel Noto <marcoemmanuelnoto@gmail.com>
2022-07-20 09:58:03 +0200

    aligned gatsby-ssr and gatsby-browser

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,15 @@
 import React from 'react';
-import CustomLayout from "./wrapPageElement"
+// custom typefaces
+import "typeface-montserrat"
+import "typeface-merriweather"
+// normalize CSS across browsers
+import "./src/normalize.css"
+// custom CSS styles
+import "./src/style.css"
 
+// Highlighting for code blocks
+import "prismjs/themes/prism.css"
+
+import CustomLayout from "./wrapPageElement"
 
 export const wrapPageElement = CustomLayout
